086f05207a6ef2b792f0cb8f8deb279c8097aff7
jrabbott2 <jabbott2@cub.uca.edu>
2025-04-04 19:06:01 -0500

    update message parsing and remove led

--- scripts/pico/main.py
+++ scripts/pico/main.py
@@ -9,39 +9,37 @@ from machine import Pin, PWM, reset
 
 # SETUP
 steering = PWM(Pin(0))
-
 steering.freq(50)
 throttle = PWM(Pin(15))
 throttle.freq(50)
-led = Pin('LED')
 
 
 # LOOP
 try:
-    led.toggle()
-    sleep(3)  # ESC calibrate
+    sleep(3)  # get pico ready
+    print("I'm listening...")
     poller = select.poll()
     poller.register(sys.stdin, select.POLLIN)
-    print("I'm listening...")
     event = poller.poll()
 
-    while True:
-        # read data from serial
-        
+    while True:        
         for msg, _ in event:
             buffer = msg.readline().rstrip().split(',')
             # print(buffer) # debug
             # print(len(buffer)) # debug
             if len(buffer) == 2:
-                ns_st, ns_th = int(buffer[0]), int(buffer[1])
-                if ns_st == ns_th == 'END':
-                    break
-                print(ns_st, ns_th) # debug
+                try:
+                    ns_st, ns_th = int(buffer[0]), int(buffer[1])
+                except ValueError:
+                    pass
+                # if ns_st == ns_th == 'END':
+                #     break
+                # print(ns_st, ns_th) # debug
                 steering.duty_ns(ns_st)
                 throttle.duty_ns(ns_th)
 
 except:
-    led.toggle()
+    pass
 finally:
     print('Pico reset')
     reset()
